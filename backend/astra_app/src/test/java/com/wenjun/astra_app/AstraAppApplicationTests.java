79de5bf9a47ce349f97d1784b6a5e071ba77ae21
Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2025-05-11 20:55:22 +0800

    Create ci.yml

--- backend/astra_app/src/test/java/com/wenjun/astra_app/AstraAppApplicationTests.java
+++ backend/astra_app/src/test/java/com/wenjun/astra_app/AstraAppApplicationTests.java
@@ -3,7 +3,6 @@ package com.wenjun.astra_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AstraAppApplicationTests {
 
 	@Test
