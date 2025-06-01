421d2f8286bf8dbadc24a74132fa5c5a19e4e40e
woowenjun99 <woowenjun99@gmail.com>
2025-06-01 17:35:30 +0800

    Remove the Flutter app

--- astra/android/app/src/main/kotlin/com/example/astra/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.astra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- astra/lib/src/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env', obfuscate: true, useConstantCase: true)
-abstract class Env {
-  @EnviedField(varName: "BASE_URL")
-  static String baseUrl = _Env.baseUrl;
-}
