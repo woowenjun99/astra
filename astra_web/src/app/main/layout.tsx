e0c80cf3831b2c60ee1c1ee647c46112fe6d3fef
woowenjun99 <woowenjun99@gmail.com>
2025-06-27 17:42:27 +0800

    Feat/Add in side bar

--- astra_web/src/app/main/layout.tsx
+++ astra_web/src/app/main/layout.tsx
@@ -1,12 +1,18 @@
 "use client";
 import ThemeToggle from "@/components/theme-toggle";
 import type { ReactNode } from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 p-6">
-      <ThemeToggle />
-      {children}
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="min-h-screen w-full bg-gray-50 dark:bg-gray-900 p-6">
+        <SidebarTrigger />
+        <ThemeToggle />
+        {children}
+      </div>
+    </SidebarProvider>
   );
 }
